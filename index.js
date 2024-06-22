1621f5c0e78f134b81384a2102fa038ac012a6c8
yan <yan.braverman37@gmail.com>
2024-06-22 14:12:11 +0300

    Add http and create server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
-const http = require('http')
+const http = require('http');
 
 const pathToTextFile = './src/test/texts';
 const pathToAudioFile = './src/test/holy_diver.mp3';
@@ -73,3 +73,51 @@ console.log(process.env.PORT)
 console.log(process.env.DB_PORT) */
 
 // console.log(sumFunc(20, 30))
+
+// Create server with http module
+const HOST_NAME = '127.0.0.1';
+const PORT = 5000;
+
+const server = http.createServer((req, res) => {
+	const url = req.url;
+	console.log(`URL is ${url}`);
+	console.log(`METHOD is ${req.method}`);
+
+	res.setHeader('Content-Type', 'text/html; charset=utf-8');
+	// res.write('<h1>SERVER</h1>')
+	// res.end()
+	switch (url) {
+		case '/':
+			console.log('Home page');
+			const homePage = fs.readFileSync('./public/index.html', 'utf8');
+			res.write(homePage);
+			res.end();
+			break;
+        case '/contact':
+            console.log('Contact page');
+            const contactPage = fs.readFileSync('./public/contact.html', 'utf8');
+			res.write(contactPage);
+			res.end();
+			break;
+        default:
+            if(url.includes('/img')){
+                console.log('images')
+                fs.readFile(`./public${url}`, (err, data) => {
+                    if(err){
+                        res.statusCode = 404;
+                        throw err;
+                    }
+                    res.setHeader('Content-Type', 'image/jpeg')
+                    res.write(data)
+                    res.end();
+                })
+            }else{
+                res.write('<h1>Page not found</h1>')
+            }
+            
+	}
+});
+
+server.listen(PORT, HOST_NAME, () =>
+	console.log(`Server running at http://${HOST_NAME}:${PORT}`)
+);
